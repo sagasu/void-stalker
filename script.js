325284f0cd8d0a929b6894a709b5a61354957bb2
Mateusz Kopij <sagasu@gmail.com>
2023-05-27 22:37:11 +0100

    introducing player

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 window.addEventListener('load', () => {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+
     canvas.width = 1280;
     canvas.height = 720;
 
     class Player {
         constructor(game){
             this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 50;
         }
 
         draw(context){
             context.beginPath();
-            context.arc(400,400,50,0,Math.PI*2);
+            context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI*2);
+            context.save();
+            context.globalAlpha = 0.5;
             context.fill();
+            context.restore();
+            context.stroke();
         }
 
         
@@ -24,7 +35,7 @@ window.addEventListener('load', () => {
             this.height = this.canvas.height;
             this.player = new Player(this);
         }
-        
+
         render(context){
             this.player.draw(context);
         }
