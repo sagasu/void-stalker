7f571526f0c2b7c66c694aa95ff196a68ed8585c
Mateusz Kopij <sagasu@gmail.com>
2023-05-28 11:26:09 +0100

    adding event listeners

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ window.addEventListener('load', () => {
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
             this.collisionRadius = 50;
+            this.speedX = 0;
+            this.speedY = 0;
         }
 
         draw(context){
@@ -24,6 +26,15 @@ window.addEventListener('load', () => {
             context.fill();
             context.restore();
             context.stroke();
+            context.beginPath();
+            context.moveTo(this.collisionX, this.collisionY);
+            context.lineTo(this.game.mouse.x, this.game.mouse.y);
+            context.stroke();
+        }
+
+        update() {
+            this.collisionX = this.game.mouse.x;
+            this.collisionY = this.game.mouse.y;
         }
 
         
@@ -34,10 +45,33 @@ window.addEventListener('load', () => {
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this);
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            };
+
+            window.addEventListener('mousedown', (e) => {
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+            });
+
+            window.addEventListener('mouseup', (e) => {
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            });
+
+            window.addEventListener('mousemove', (e) => {
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+            });
         }
 
         render(context){
             this.player.draw(context);
+            this.player.update();
         }
     }
 
@@ -45,6 +79,10 @@ window.addEventListener('load', () => {
     game.render(ctx);
 
     function animate(){
-
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        game.render(ctx);
+        window.requestAnimationFrame(animate);
     }
+
+    animate();
 });
