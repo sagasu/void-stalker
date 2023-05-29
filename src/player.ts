47ce66d3ad8854d83a1ed804523397c98d225ce6
Mateusz Kopij <sagasu@gmail.com>
2023-05-29 20:17:05 +0100

    player now has an image

--- src/player.ts
+++ src/player.ts
@@ -11,6 +11,13 @@ export class Player {
     dx: number;
     dy: number;
     speedModifier: number;
+    image: CanvasImageSource;
+    spriteWidth: number;
+    spriteHeight: number;
+    width: any;
+    height: number;
+    spriteX: number;
+    spriteY: number;
 
     constructor(game: Game) {
         this.game = game;
@@ -22,9 +29,21 @@ export class Player {
         this.dx = 0;
         this.dy = 0;
         this.speedModifier = 5;
+
+        this.spriteWidth = 255;
+        this.spriteHeight = 255;
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
+
+        this.spriteX = this.collisionX - this.width * 0.5;
+        this.spriteY = this.collisionY - this.height * 0.5 - 100;
+
+        this.image = <CanvasImageSource>document.getElementById('bull');
     }
 
     draw(context: CanvasRenderingContext2D) {
+        context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
+
         context.beginPath();
         context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
         context.save();
@@ -45,7 +64,7 @@ export class Player {
         if(distance > this.speedModifier) {
             this.speedX = this.dx / distance || 0;
             this.speedY = this.dy / distance || 0;
-        }else{
+        } else {
             this.speedX = 0;
             this.speedY = 0;
         }
@@ -53,13 +72,19 @@ export class Player {
         this.collisionX += this.speedX * this.speedModifier;
         this.collisionY += this.speedY * this.speedModifier;
 
+        this.spriteX = this.collisionX - this.width * 0.5;
+        this.spriteY = this.collisionY - this.height * 0.5  - 100;
+
         this.game.obstacles.forEach(obstacle => {
             let [isCollision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, obstacle);
             if(isCollision){
-
+                const unitX = dx/ distance;
+                const unitY = dy /distance;
+                this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unitX;
+                this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unitY;
             }
         });
     }
-
-
 }
+
+
