9308bcf29689b6cd43b235b614592d7088ca65b2
Mateusz Kopij <sagasu@gmail.com>
2023-05-30 11:13:34 +0100

    sprites are changing for player depending on movement direction

--- src/player.ts
+++ src/player.ts
@@ -18,6 +18,8 @@ export class Player {
     height: number;
     spriteX: number;
     spriteY: number;
+    frameX: number;
+    frameY: number;
 
     constructor(game: Game) {
         this.game = game;
@@ -38,11 +40,14 @@ export class Player {
         this.spriteX = this.collisionX - this.width * 0.5;
         this.spriteY = this.collisionY - this.height * 0.5 - 100;
 
+        this.frameX = 0;
+        this.frameY = 5;
+
         this.image = <CanvasImageSource>document.getElementById('bull');
     }
 
     draw(context: CanvasRenderingContext2D) {
-        context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
 
         context.beginPath();
         context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
@@ -58,8 +63,23 @@ export class Player {
     }
 
     update() {
+        
+
         this.dx = this.game.mouse.x - this.collisionX;
         this.dy = this.game.mouse.y - this.collisionY;
+
+        const angle = Math.atan2(this.dy, this.dx);
+        
+        if(angle < -2.74 || angle > 2.74) this.frameY = 6;
+        else if(angle < -1.96) this.frameY = 7;
+        else if(angle < -1.17) this.frameY = 0;
+        else if(angle < -0.39) this.frameY = 1;
+        else if(angle < 0.39) this.frameY = 2;
+        else if(angle < 1.17) this.frameY = 3;
+        else if(angle < 1.96) this.frameY = 4;
+        else if(angle < 2.74) this.frameY = 5;
+        
+
         const distance = Math.hypot(this.dy, this.dx);
         if(distance > this.speedModifier) {
             this.speedX = this.dx / distance || 0;
