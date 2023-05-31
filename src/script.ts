f5198698600142611d3b9193c7903e3e1035d6c2
Mateusz Kopij <sagasu@gmail.com>
2023-05-31 20:32:24 +0100

    changing color

--- src/script.ts
+++ src/script.ts
@@ -3,12 +3,12 @@ import {Game} from './game.js';
 window.addEventListener('load', () => {
     const canvas = <HTMLCanvasElement>document.getElementById('canvas1');
     const ctx = canvas.getContext('2d')!;
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'black';
     ctx.lineWidth = 3;
-    ctx.strokeStyle = 'white';
+    ctx.strokeStyle = 'black';
 
     canvas.width = 1280;
-    canvas.height = 720;
+    canvas.height = 720; 
 
     const game = new Game(canvas);
     game.init();
